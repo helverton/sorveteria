f1e65d70c8b812acba5ecf940cf63702f949d387
Helverton <helvertonsantos@gmail.com>
2016-03-28 19:41:13 -0300

    Sorveteria OK

--- app/models/cardapio.rb
+++ app/models/cardapio.rb
@@ -1,4 +1,4 @@
 class Cardapio < ActiveRecord::Base
-	has_attached_file :image, styles: { larg: "600x600", medium: "300x300>", thumb: "100x100>" }, default_url: "default.png"
+	has_attached_file :image, styles: { larg: "600x600", medium: "300x300>", thumb: "100x100>" }, default_url: "http://sorvetesnectar.com/wp-content/uploads/2012/12/sorvetes.gif"
  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
